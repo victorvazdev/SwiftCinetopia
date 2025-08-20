92d80217af187f859f2c8d4aa0ccace122efc538
Victor Vaz <contact@victorvaz.com>
2025-08-20 12:06:39 -0300

    Initial Commit

---
+++ Cinetopia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cinetopia
+//
+//  Created by Victor Vaz de Oliveira on 20/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
