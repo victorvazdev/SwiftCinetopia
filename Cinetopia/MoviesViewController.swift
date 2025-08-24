58afa541a63fa95cc52cee19ed376aff86f55bf7
Victor Vaz <contact@victorvaz.com>
2025-08-24 16:16:19 -0300

    Table view created in MoviesViewController

--- Cinetopia/MoviesViewController.swift
+++ Cinetopia/MoviesViewController.swift
@@ -7,12 +7,48 @@
 
 import UIKit
 
-class MoviesViewController: UIViewController {
+class MoviesViewController: UIViewController, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    var names: Array<String> = [
+        "Victor", "Ana", "Lucas", "Giovana", "Daniel"
+    ]
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = .clear
+        tableView.dataSource = self
+        
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .background
         setupNavigationBar()
+        setupConstraints()
+        addSubviews()
+    }
+    
+    private func setupConstraints() {
+        view.addSubview(tableView)
+    }
+    
+    private func addSubviews() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
     }
     
     private func setupNavigationBar() {
